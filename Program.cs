d4d4a2cc88e5c409a0fe8271f61e2f42750b5636
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-08 07:51:33 +0530

    ADD: Main Program File Added

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+{ 
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Username.DisplayUsername();
+
+
+            //LEAP-YEAR Task Day1
+
+            //LeapYear.LeapYearshow();
+
+
+
+            // FLIPCOIN Day2
+
+            FlipCoin.CalculatePercentage();
+
+
+        }
+    }
+}
