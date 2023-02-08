5e7f6446ed0b8597ae33a501b04b2c7d8e477d03
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-08 08:17:23 +0530

    ADD: Program File to Calculate Percentage Added

---
+++ FlipCoin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class FlipCoin
+    {
+        public static void CalculatePercentage()
+        {
+            Console.WriteLine("Please Enter the value for number of flips");
+
+            int head = 0;
+            int tail = 0; 
+            int headcount = 0;
+            int tailcount = 0;
+
+            double HeadPercentage;
+            double tailPercentage;
+
+            int flipCoin = Convert.ToInt32(Console.ReadLine());
+
+            if(flipCoin>0)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Please Enter a Valid Number ");
+            }
+
+        }
+    }
+}
